04b19f087f71c598307e339d10a3b8fcadeb73aa
harshakakarla77 <37071170+harshakakarla77@users.noreply.github.com>
2019-08-11 23:35:21 +0530

    Update max.c

--- max.c
+++ max.c
@@ -1,17 +1,23 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 void main()
 {
-int arr[100],n,max;
-printf("Enter the number of elements in the array:");
-scanf("%d",&n);
-printf("Enter the array elements:");
-for(int i=0;i<n;i++)
-scanf("%d",&arr[i]);
-max=arr[0];
-for(int i=0;i<n;i++)
-{
-if(arr[i]>max)
-max=arr[i];
-}
-printf("The largest element in the array is: %d",max);
+  int arr[100],n,max;
+  printf("Enter the number of elements in the array:");
+  scanf("%d",&n);
+  printf("The array elements are:");
+  for(int i=0;i<n;i++)
+  {
+  arr[i]=rand()%50;
+  printf("%d",arr[i]);
+  printf("\n");
+  }
+  max=arr[0];
+  for(int i=0;i<n;i++)
+  {
+  if(arr[i]>max)
+  max=arr[i];
+  }
+  printf("The largest element in the array is: %d",max);
 }
